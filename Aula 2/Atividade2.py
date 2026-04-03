7b646840991a856f1026b99932047a12af1c3225
silviacpmachado-sketch <silviacpmachado@gmail.com>
2026-04-02 21:28:39 -0300

    Altera repositorio

---
+++ Aula 2/Atividade2.py
@@ -0,0 +1,9 @@
+numero=int(input('digite um numero: '))
+dobro=numero * 2
+triplo=numero * 3
+quadrado=numero **2
+
+print(f'Dobro: {dobro}')
+print(f'Triplo: {triplo}')
+print(f'Quadrado: {quadrado}')
+
